e905b4f1b66fc1893f0983c4cd2975788091a34a
kangho lee <ibcylon@naver.com>
2023-09-03 18:10:23 +0900

    Initial Commit

---
+++ RickAndMortyDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyDB
+//
+//  Created by Kanghos on 2023/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
