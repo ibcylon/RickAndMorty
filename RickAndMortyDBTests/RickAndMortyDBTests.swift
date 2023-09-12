8c0ecd3d836a99b8707659b61c4d90c285d0b6b4
kangho lee <ibcylon@naver.com>
2023-09-12 18:54:33 +0900

    Test: API Test

--- RickAndMortyDBTests/RickAndMortyDBTests.swift
+++ RickAndMortyDBTests/RickAndMortyDBTests.swift
@@ -24,6 +24,22 @@ final class RickAndMortyDBTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+      let expectation = XCTestExpectation(description: "api test")
+      let session = URLSession.shared
+      let request = RequestWithPath(
+        base: RequestWithURL(url: URL(string: Constant.baseURLString)!),
+        path: [EndPoint.character.rawValue]).request()
+      session.perform(request) { result in
+        switch result {
+        case .success(let success):
+          print(success.1.statusCode)
+          expectation.fulfill()
+        case .failure(let failure):
+          print(failure)
+          expectation.fulfill()
+        }
+      }
+      wait(for: [expectation], timeout: 10)
     }
 
     func testPerformanceExample() throws {
