e1db25c28c4d79ee96cfaedf9de8b2cb7d0d6a3f
kangho lee <ibcylon@naver.com>
2023-10-31 20:39:19 +0900

    Add: CharacterDetail Scene

--- RickAndMortyDB/Presentation/Character/Detail/CharacterDetailVIewController.swift
+++ RickAndMortyDB/Presentation/Character/Detail/CharacterDetailVIewController.swift
@@ -7,29 +7,73 @@
 
 import UIKit
 
+import SnapKit
+import RxCocoa
+import RxSwift
+
 final class CharacterDetailVIewController: UIViewController {
   
-  var viewModel: CharacterDetailViewModel!
-
-  init() {
+  var viewModel: CharacterDetailViewModel
+  var disposeBag = DisposeBag()
+  private lazy var characterDetailView = RMCharacterDetailView()
+  init(viewModel: CharacterDetailViewModel) {
+    self.viewModel = viewModel
     super.init(nibName: nil, bundle: nil)
-    setUpViews()
-    bind()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+
+  deinit {
+    RMLogger.dataLogger.debug("\(#function) \(self)")
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    setUpViews()
+    bind()
   }
 
   private func setUpViews() {
+    self.navigationItem.largeTitleDisplayMode = .never
     self.view.backgroundColor = .white
+    self.view.addSubview(characterDetailView)
+
+    characterDetailView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
   }
 
   func bind() {
+    let backButton = UIBarButtonItem(
+      image: UIImage(systemName: "chevron.left"),
+      style: .plain,
+      target: nil,
+      action: nil)
+    self.navigationItem.leftBarButtonItem = backButton
 
+    let backbuttonTrigger = backButton.rx.tap
+      .asDriver()
+    let trigger = self.rx.viewWillAppear.map { _ in }
+      .asDriver(onErrorDriveWith: .empty())
+
+    let input = CharacterDetailViewModel.Input(
+      trigger: trigger,
+      backButtonTrigger: backbuttonTrigger
+    )
+    let output = viewModel.transform(input: input)
+
+    output.character
+      .drive(onNext: { [weak self] item in
+        self?.navigationItem.title = item.name
+        self?.characterDetailView.bind(item: item)
+      })
+      .disposed(by: disposeBag)
+
+    output.toList
+      .drive()
+      .disposed(by: disposeBag)
   }
 }
--- RickAndMortyDB/Presentation/Character/Detail/CharacterDetailViewModel.swift
+++ RickAndMortyDB/Presentation/Character/Detail/CharacterDetailViewModel.swift
@@ -7,22 +7,58 @@
 
 import Foundation
 
-final class CharacterDetailViewModel: ViewModelType {
-  private let navigator: CharacterDetailNavigatorType
+import RxCocoa
+import RxSwift
 
-  init(navigator: CharacterDetailNavigatorType) {
-    self.navigator = navigator
+protocol CharacterDetailDelegate: AnyObject {
+  func toList()
+}
+
+final class CharacterDetailViewModel: ViewModelType {
+  private let service: RMCharacterService
+  private let model: RMCharacter  
+  init(
+    service: RMCharacterService,
+    model: RMCharacter
+  ) {
+    self.service = service
+    self.model = model
   }
+
+  var delegate: CharacterDetailDelegate?
   
   struct Input {
-
+    let trigger: Driver<Void>
+    let backButtonTrigger: Driver<Void>
   }
 
   struct Output {
-
+    let character: Driver<RMCharacter>
+    let toList: Driver<Void>
+    let error: Driver<Error>
   }
   
   func transform(input: Input) -> Output {
-    return Output()
+    let initialCharacter = Driver.just(model)
+    let errorSubject = PublishSubject<Error>()
+    let character = input.trigger.withLatestFrom(initialCharacter)
+      .flatMapLatest { [unowned self] initial in
+        service.getSingleItem(id: "\(initial.id)")
+          .do(onError: { error in
+            errorSubject.onNext(error)
+          })
+          .asDriverOnErrorJustComplete()
+      }
+    let toList = input.backButtonTrigger
+      .do(onNext: { [weak self] in
+        self?.delegate?.toList()
+      })
+
+    let error = errorSubject.asDriverOnErrorJustComplete()
+    return Output(
+      character: character,
+      toList: toList,
+      error: error
+    )
   }
 }
