3bb06a4638cf6714b1064efa109cd628a489b8cb
kangho lee <ibcylon@naver.com>
2023-10-31 20:44:51 +0900

    Feat: Character Paging API

--- RickAndMortyDB/Network/Target/CharacterTarget.swift
+++ RickAndMortyDB/Network/Target/CharacterTarget.swift
@@ -12,6 +12,7 @@ import RxSwift
 protocol RMServiceType {
   func getAllItems() -> Observable<RMInfoModel<RMCharacter>>
   func getNextPage(with nextPageURL: URL) -> Observable<RMInfoModel<RMCharacter>>
+  func getSingleItem(id: String) -> Observable<RMCharacter>
 }
 
 class RMCharacterService: RMServiceType {
@@ -30,6 +31,12 @@ class RMCharacterService: RMServiceType {
   func getNextPage(with nextPageURL: URL) -> Observable<RMInfoModel<RMCharacter>> {
     let request = RequestWithURL(url: nextPageURL).request()
     return performInfoList(request)
+  }
+
+  func getSingleItem(id: String) -> Observable<RMCharacter> {
+    let endPoint = target.url.appending(path: id)
+    let request = RequestWithURL(url: endPoint).request()
+    return performItem(request)
   }
 
   private func performInfoList(_ request: URLRequest) -> Observable<RMInfoModel<RMCharacter>> {
@@ -51,6 +58,27 @@ class RMCharacterService: RMServiceType {
       }
     }
   }
+
+  private func performItem<T: Codable>(_ request: URLRequest) -> Observable<T> {
+    return .create { [weak self] observer in
+      self?.client.perform(request) { result in
+        switch result {
+        case .success(let response):
+          do {
+            let item = try RMMapper<T>.map(data: response.0, response: response.1)
+            observer.onNext(item)
+          } catch {
+            observer.onError(error)
+          }
+        case .failure(let failure):
+          observer.onError(failure)
+        }
+      }
+      return Disposables.create {
+        observer.onCompleted()
+      }
+    }
+  }
 }
 
 struct RMInfoMapper<T: Codable> {
@@ -69,15 +97,15 @@ struct RMInfoMapper<T: Codable> {
 }
 
 struct RMMapper<T: Codable> {
-  static func map(data: Data, response: HTTPURLResponse) throws -> [T] {
-    if response.statusCode == 204 {
-      return []
-    }
+  static func map(data: Data, response: HTTPURLResponse) throws -> T {
+//    if response.statusCode == 204 {
+//      return
+//    }
 
     if response.statusCode == 200 {
       do {
-        let json = try JSONDecoder().decode(RMInfoModel<T>.self, from: data)
-        return json.results
+        let json = try JSONDecoder().decode(T.self, from: data)
+        return json
       } catch {
         throw InvalidHTTPResponseError()
       }
@@ -85,16 +113,16 @@ struct RMMapper<T: Codable> {
 
     if response.statusCode == 401 {
       print("401 Unauthroized")
-      return []
+      throw InvalidHTTPResponseError()
     }
 
     if response.statusCode == 403 {
       print("403 forbidden")
-      return []
+      throw InvalidHTTPResponseError()
     }
 
-    print(response.statusCode)
-    print(response.description)
-    return []
+    RMLogger.dataLogger.error("\(response.statusCode)")
+    RMLogger.dataLogger.error("\(response.description)")
+    throw InvalidHTTPResponseError()
   }
 }
